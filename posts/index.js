94ac84d70ed480e205b0dec83fefd3c336030006
Israel Torres <israel.a.torres01@gmail.com>
2021-06-05 14:33:40 -0400

    first commit

---
+++ posts/index.js
@@ -0,0 +1,57 @@
+const express = require('express')
+const { randomBytes } = require('crypto')
+const cors = require('cors')
+const axios = require('axios')
+
+const app = express()
+app.use(express.json())
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+)
+app.use(cors())
+
+const posts = {}
+
+app.get('/posts', (req, res) => {
+  try {
+    res.send(posts)
+  } catch (error) {
+    console.log(error)
+  }
+})
+
+app.post('/posts', async (req, res) => {
+  try {
+    const id = randomBytes(4).toString('hex')
+    const { title } = req.body
+
+    posts[id] = {
+      id,
+      title,
+    }
+
+    await axios.post('http://localhost:4005/events', {
+      type: 'PostCreated',
+      data: {
+        id,
+        title,
+      },
+    })
+
+    res.status(201).send(posts[id])
+  } catch (error) {
+    console.log(error)
+  }
+})
+
+app.post('/events', (req, res) => {
+  console.log('Received Event', req.body.type)
+
+  res.send({})
+})
+
+app.listen(4000, () => {
+  console.log('Listening on 4000')
+})
